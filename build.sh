56f21605cded8fb71b00e0fc66d8e72b0cd01d7a
Jerod Santo <jerod.santo@gmail.com>
2025-10-23 10:09:15 -0500

    Only generate image files when source changes

--- build.sh
+++ build.sh
@@ -4,17 +4,20 @@ ICON=icon.png
 FAVICON=favicon.png
 
 # Generate different sizes from a source image
-magick ${SRC}/${ICON} -resize 120x120 ${DST}/apple-touch-icon-120x120.png
-magick ${SRC}/${ICON} -resize 152x152 ${DST}/apple-touch-icon-152x152.png
-magick ${SRC}/${ICON} -resize 180x180 ${DST}/apple-touch-icon.png
-
-magick ${SRC}/${ICON} -resize 192x192 ${DST}/android-chrome-192x192.png
-magick ${SRC}/${ICON} -resize 512x512 ${DST}/android-chrome-512x512.png
+if [ "${SRC}/${ICON}" -nt "${DST}/apple-touch-icon.png" ]; then
+  magick ${SRC}/${ICON} -resize 120x120 ${DST}/apple-touch-icon-120x120.png
+  magick ${SRC}/${ICON} -resize 152x152 ${DST}/apple-touch-icon-152x152.png
+  magick ${SRC}/${ICON} -resize 180x180 ${DST}/apple-touch-icon.png
+  magick ${SRC}/${ICON} -resize 192x192 ${DST}/android-chrome-192x192.png
+  magick ${SRC}/${ICON} -resize 512x512 ${DST}/android-chrome-512x512.png
+fi
 
 # Create .ico file with multiple resolutions
-magick ${SRC}/${FAVICON} -resize 16x16 ${DST}/favicon-16x16.png
-magick ${SRC}/${FAVICON} -resize 32x32 ${DST}/favicon-32x32.png
-magick ${SRC}/${FAVICON} -define icon:auto-resize=64,48,32,16 ${DST}/favicon.ico
+if [ "${SRC}/${FAVICON}" -nt "${DST}/favicon-16x16.png" ]; then
+  magick ${SRC}/${FAVICON} -resize 16x16 ${DST}/favicon-16x16.png
+  magick ${SRC}/${FAVICON} -resize 32x32 ${DST}/favicon-32x32.png
+  magick ${SRC}/${FAVICON} -define icon:auto-resize=64,48,32,16 ${DST}/favicon.ico
+fi
 
 # Symlink static files to dist (only if they don't exist)
 for file in static/*; do
